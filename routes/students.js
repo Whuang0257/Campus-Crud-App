2241f8c7a54e1b93d99de0508ff2f5caf275e3f8
ianzures <i.anzures514@gmail.com>
2020-07-15 19:49:25 -0400

    Remove extraneous comments and added check for proper gpa. No check for email yet.

--- routes/students.js
+++ routes/students.js
@@ -2,67 +2,69 @@ var express = require("express");
 var router = express.Router();
 const { Student } = require("../database/models/student");
 
-/* GET all campuses. */
-// /api/campuses
+/* GET all students. */
 router.get("/", async (req, res, next) => {
-    // try to get campuses object from database
     try {
-        // campuses will be the result of the Campus.findAll promise
         const students = await Student.findAll();
-        // if campuses is valid, it will be sent as a json response
         res.status(200).json(students);
     } catch (err) {
-        // if there is an error, it'll passed via the next parameter to the error handler middleware
         next(err);
     }
 });
 
-// Route to serve single campus based on its id
-// /api/campuses/:id
-// /api/campuses/456 would respond with a campus with id 456
+// Route to serve single student based on its id
+// /api/student/456 would respond with a student with id 456
 router.get("/:id", async (req, res, next) => {
     // take the id from params
     const { id } = req.params;
-    // query the database for a campus with matching id
     try {
-        // if successful:
         const student = await Student.findByPk(id);
-        // send back the campus as a response
         res.status(200).json(student);
     } catch (err) {
-        // if error:
-        // handle error
         next(err);
     }
 });
 
-// Route to handle adding a campus
-// /api/campuses/
+// Route to handle adding a student 
 router.post("/", async (req, res, next) => {
+    let errors = [];
     // Take the form data from the request body
     const { fName, lName, email, imageUrl, gpa } = req.body;
-    // Create a campus object
-    const studentObj = {
-        firstName: fName,
-        lastName: lName,
-        email: email,
-        imageUrl: imageUrl,
-        gpa: gpa,
-    };
-    try {
-        // Create a new campus on the database
-        const newStudent = await Student.create(studentObj);
-        // The database would return a campus
-        // send that campus as a json to the client
-        res.status(201).send(newStudent);
-    } catch (err) {
-        next(err);
+
+    if (!(0 >= gpa <= 4.0)) {
+        errors.push({ text: 'Please enter proper gpa' });
+    }
+
+        // Check for errors
+    if (errors.length > 0) {
+        res.render('add', {
+            errors, name, imageUrl, address, description
+        });
+    } else {
+
+        // Create a campus object
+        const studentObj = {
+            firstName: fName,
+            lastName: lName,
+            email: email,
+            imageUrl: imageUrl,
+            gpa: gpa,
+        };
+
+        try {
+            // Create a new campus on the database
+            const newStudent = await Student.create(studentObj);
+            // The database would return a campus
+            // send that campus as a json to the client
+            res.status(201).send(newStudent);
+        } catch (err) {
+            next(err);
+        }
     }
 });
 
-// Route to handle editing a campus
-// /api/campuses/:id
-// /api/campuses/456 would modify a campus with id 456
+// Route to handle editing a student
+// /api/student/456 would modify a student with id 456
 router.put("/:id", async (req, res, next) => {
     // get the id from request params
     const { id } = req.params;
@@ -76,35 +78,29 @@ router.put("/:id", async (req, res, next) => {
         gpa: gpa,
     };
     try {
-        // if successfull:
-        // Find a campus with a matching id from the database
+        // Find a student with a matching id from the database
         const student = await Student.findByPk(id);
-        // database would return a valid campus object or an error
+        // database would return a valid student object or an error
         console.log(updatedObj);
-        // modify the campus object with new form data
+        // modify the student object with new form data
         await student.set(updatedObj);
-        // save the new campus object to the data
-        // database would return a new campus object
+        // save the new student object to the data
+        // database would return a new student object
         const updatedStudent = await student.save();
         console.log(updatedStudent);
-        // send the newCampus as a response from the API
+        // send the newStudent as a response from the API
         res.status(201).send(updatedStudent);
     } catch (err) {
-        // if error:
-        // handle the error
         next(err);
     }
 });
 
-// Route to handle removing a campus
+// Route to handle removing a student
 router.delete("/:id", async (req, res, next) => {
     const { id } = req.params;
-    // get an id for a campus to delete
     try {
-        // pass the id to the database to find campus to be deleted
         // database would either respond succcess or fail
         const student = await Student.findByPk(id);
-        // invoke the .destroy() method on the returned campus
         await student.destroy();
         // send a success message to the client
         res.sendStatus(204);
