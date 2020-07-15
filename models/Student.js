a49068ae536827b2840a7677d3396d956153c75a
ianzures <i.anzures514@gmail.com>
2020-07-15 18:10:59 -0400

    Student model no longer allows nulls and has a default imageUrl.

--- models/Student.js
+++ models/Student.js
@@ -3,19 +3,25 @@ const db = require('../config/database');
 
 const Student = db.define('student', {
 	firstName: {
-		type: Sequelize.STRING
+		type: Sequelize.STRING,
+		allowNull: false
 	},
 	lastName: {
-		type: Sequelize.STRING
+		type: Sequelize.STRING,
+		allowNull: false
 	},
 	email: {
-		type: Sequelize.STRING
+		type: Sequelize.STRING,
+		allowNull: false
 	},
 	imageUrl: {
-		type: Sequelize.STRING
+		type: Sequelize.STRING,
+		defaultValue: 'http://fairlaneclub.com/wp-content/uploads/2013/12/generic_person_silhouette_sq.jpg'
 	},
 	gpa: {
-		type: Sequelize.DECIMAL(2,1)
+		type: Sequelize.DECIMAL(2, 1),
+		allowNull: false
+
 	}
 });
 module.exports = Student;
