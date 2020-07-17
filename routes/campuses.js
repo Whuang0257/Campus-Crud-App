0ab86294427adee79fe0faba772a5d21dbe73dd3
Weiming Huang <weiming.huang36@myhunter.cuny.edu>
2020-07-16 23:01:09 -0400

    Finished campus and student
    Finished for both student and campus:
    addition/deletion/edit/update
    
    Checks for valid values or redirects you back for edit page.

--- routes/campuses.js
+++ routes/campuses.js
@@ -2,21 +2,86 @@ const express = require('express');
 const router = express.Router();
 const db = require('../config/database');
 const Campus = require('../models/Campus');
-const Sequelize = require('sequelize');
 
-
+//renders all campuses
 router.get('/', (req, res) => 
   Campus.findAll()
     .then(campuses => res.render('campuses', {
       campuses
       }))
-    .catch(err => res.render('error', {error: err})));
+    .catch(err => res.render('error', {error: err}))
+);
 
+//view a specific campus
+router.get('/view/:id', (req, res) => {
+  Campus.findByPk(req.params.id)
+    .then(campus => {
+      res.render('viewcampus', {
+      campuses: [campus]
+      })})
+    .catch(err => res.render('error', {error: err}))
+});
 
+//renders campus edit
+router.get('/view/:id/editcampus', (req, res) => {
+  Campus.findByPk(req.params.id)
+    .then(campus => {
+      res.render('editcampus', {
+      campuses: [campus]
+      })})
+    .catch(err => res.render('error', {error: err}))
+});
+
+//edit a campus and sends info
+router.post('/view/:id/editcampus', (req, res) => {
+  let {name, imageUrl, address, description} = req.body;
+  let id = req.params.id;
+  let errors = [];
+
+  // Validate Fields
+  if(!name) {
+    errors.push({ text: 'Please add a name' });
+  }
+  if(!address) {
+    errors.push({ text: 'Please add a address' });
+  }
+  if(!imageUrl) {
+    imageUrl = 'https://via.placeholder.com/140x100';
+  }
+
+  if(errors.length > 0) {
+    res.redirect((`/campuses/view/${req.params.id}/editcampus`));
+  } else {
+    Campus.update({
+      name: req.body.name,
+      imageUrl: req.body.imageUrl,
+      address: req.body.address,
+      description: req.body.description
+    },{
+      where: {
+        id: req.params.id
+      }
+    }).then(campus => res.redirect(`/campuses/view/${req.params.id}`))
+    .catch(err => res.render('error', {error: err}))
+  }
+});
+
+//delete a campus
+router.delete('/view/:id', (req, res) => {
+  Campus.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(() => res.redirect('/campuses'))
+    .catch(err => res.render('error', {error: err}))
+});
+
+//loads add campus page
 router.get('/addcampus', (req, res) => res.render('addcampus'));
 
+//add a campus
 router.post('/addcampus', (req, res) => {
-  let { name, imageUrl, address, description } = req.body;
+  let {name, imageUrl, address, description} = req.body;
   let errors = [];
 
   // Validate Fields
